42076ed8d4dc48b4985baaf4f3b3e5ff0a376ecd
Jesus Montes <montesdavid88@gmail.com>
2021-03-03 19:51:02 -0800

    it works! muahaha

--- actions/actions.py
+++ actions/actions.py
@@ -18,25 +18,19 @@ class HealthForm(FormValidationAction):
         domain: "DomainDict",
     ) -> List[Text]:
         custom_slots = ["confirm_exercise"]
-        if all(s in slots_mapped_in_domain for s in custom_slots):
-            return slots_mapped_in_domain
-
         required_slots = custom_slots + slots_mapped_in_domain
         return required_slots
 
     async def extract_confirm_exercise(
             self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
     ) -> Dict[Text, Any]:
-        # todo - figure out why this keeps running even when the slot is filled
-        print("Confirm Exercise Extractor is running")
-        print(tracker.slots)
         if tracker.get_intent_of_latest_message() == 'request_health_form':
-            return {"confirm_exercise": None}
+            return {}
         last_bot_event = next(e for e in reversed(tracker.events) if e['event'] == 'bot')
         if last_bot_event['metadata']['template_name'] != 'utter_ask_confirm_exercise':
-            return {"confirm_exercise": None}
+            return {}
 
         if tracker.get_intent_of_latest_message() == 'affirm':
             return {"confirm_exercise": True}
         else:
-            return {"confirm_exercise": False, "exercise": None}
+            return {"confirm_exercise": False, "exercise": "None"}
